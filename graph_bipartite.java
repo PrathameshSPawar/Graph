a4d3a32928d6c4269a2537e48075f394bee33a13
Prathamesh Pawar <163292716+PrathameshSPawar@users.noreply.github.com>
2025-09-18 17:54:12 +0530

    Update graph_bipartite.java

--- graph_bipartite.java
+++ graph_bipartite.java
@@ -1,4 +1,5 @@
 //https://leetcode.com/problems/is-graph-bipartite/description/
+//BFS SOLUTION
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n=graph.length;
@@ -26,5 +27,45 @@ class Solution {
             }
         }
         return true;        
+    }
+}
+
+
+//DFS SOLUTION
+class Solution {
+    public boolean isBipartite(int V, int[][] edges) {
+        // Code here
+        int color[]=new int[V];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0 ;i<V;i++) {
+            adj.add(new ArrayList<Integer>());
+        }
+        
+        for(int i[] : edges) {
+            adj.get(i[0]).add(i[1]);
+            adj.get(i[1]).add(i[0]);
+        }
+        Arrays.fill(color,-1);
+        for(int i=0;i<V;i++) {
+            if(color[i]==-1) {
+                if(dfs(i,0,adj,color)==false) {
+                    return false;
+                };
+            }
+        }
+        return true;
+    }
+    
+    public boolean dfs(int node,int col,ArrayList<ArrayList<Integer>> adj,int color[]) {
+        color[node]=col;
+        //color[node]=1-col;
+        for(int i : adj.get(node)) {
+            if(color[i]==-1 ) {
+                if(dfs(i,1-col,adj,color)==false) return false;
+            }else if(color[i]==col) {
+                return false;
+            }
+        }
+        return true;
     }
 }
